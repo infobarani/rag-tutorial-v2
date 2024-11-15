269448060d85414743b38166ba3d861f2a3803fe
Barani <infobarani@gmail.com>
2024-11-15 14:45:10 +0800

    Changed embeddding to bge-large, model to qwen2.5

--- query_data.py
+++ query_data.py
@@ -44,7 +44,7 @@ def query_rag(query_text: str):
     prompt = prompt_template.format(context=context_text, question=query_text)
     # print(prompt)
 
-    model = Ollama(model="llama3.2")
+    model = Ollama(model="qwen2.5")
     response_text = model.invoke(prompt)
 
     sources = [doc.metadata.get("id", None) for doc, _score in results]
